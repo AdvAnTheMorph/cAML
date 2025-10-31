252e8bd4be4a9a63d2545934dcbde0ac0fcb99c7
wiese <wiese@cl.uni-heidelberg.de>
2025-10-31 14:45:44 +0100

    Consider index in dataset for hash value

--- analogical_modeling/utils.py
+++ analogical_modeling/utils.py
@@ -1,5 +1,4 @@
-"""Utility functions for reading data.
-
+"""Utility classes for reading data.
 
 remains from weka:
 - Attributes = columns
@@ -12,20 +11,26 @@ import pandas as pd
 
 
 class Instance(pd.Series):
-    _metadata = ["class_index", "ignored"]
+    _metadata = ["class_index", "ignored", "real_data", "data_idx"]
 
-    # FIXME: https://pandas.pydata.org/docs/development/extending.html
-    def __init__(self, data: pd.Series, class_index: int, ignore_list: list[str]):
+    def __init__(self, data: pd.Series, class_column: str, ignore_list: list[str], idx: int):
         super().__init__(data)
-        self.class_index = class_index
-        self.ignored = ignore_list
+        self.drop(labels=ignore_list, inplace=True)
+        self.class_index = self.keys().get_loc(class_column)
+        self.real_data = data
+        self.data_idx = idx
 
     def is_missing(self, idx: int) -> bool:
         # return self.isna().iloc[idx]
-        return self.index[idx] in self.ignored or self.iloc[idx] == "="
+        return self.iloc[idx] == "="
 
     def num_attributes(self) -> int:
+        """Return the number of considered attributes."""
         return self.shape[0]
+
+    def num_all_attributes(self) -> int:
+        """Return the number of attributes, including ignored ones."""
+        return self.real_data.shape[0]
 
     def attribute_name(self, idx: int):
         return self.index[idx]
@@ -43,7 +48,7 @@ class Instance(pd.Series):
         return f"{','.join(map(str, self.array))}"  #,\u007b{self.num_attributes()}\u007d"
 
     def __hash__(self):
-        return int(pd.util.hash_pandas_object(self, index=False).sum())
+        return int(pd.util.hash_pandas_object(self, index=False).sum())+hash(37*self.data_idx)
 
     def __eq__(self, other):
         if isinstance(other, Instance):
@@ -63,7 +68,7 @@ class Dataset:
 
     def from_csv(self, source: str|Path):
         self.data = pd.read_csv(Path(__file__).parent / source)
-        self.class_index = self.num_attributes() - 1  #self.data.columns.get_loc("class")
+        self.class_index = self.num_attributes() - 1
         return self
 
     def get_instance(self, idx):
@@ -71,6 +76,9 @@ class Dataset:
 
     def num_attributes(self) -> int:
         return self.data.shape[1]
+
+    def num_counted_attributes(self) -> int:
+        return self.data.shape[1] - len(self.ignored)
 
     def set_class_index(self, idx: int):
         if idx > self.num_attributes():
@@ -86,15 +94,18 @@ class Dataset:
     def num_classes(self):
         return len(set(self.data.iloc[:, self.class_index]))
 
+    def class_column_name(self):
+        return self.data.columns[self.class_index]
+
     def set_ignored(self, ignore: list[str]):
         self.ignored = ignore
 
     def __getitem__(self, idx) -> Instance:
-        return Instance(self.data.iloc[idx], self.class_index, self.ignored)
+        return Instance(self.data.iloc[idx], self.class_column_name(), self.ignored, idx)
 
     def __iter__(self):
-        for _, el in self.data.iterrows():
-            yield Instance(el, self.class_index, self.ignored)
+        for idx, el in self.data.iterrows():
+            yield Instance(el, self.class_column_name(), self.ignored, idx)
 
     def __len__(self):
         return self.data.shape[0]
