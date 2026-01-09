d1ddd0e845b7d738a8db686226fa3b0914fc076c
wiese <wiese@cl.uni-heidelberg.de>
2026-01-09 14:58:52 +0100

    Set default saving filetype to csv

--- analogical_modeling/am/gui/vis.py
+++ analogical_modeling/am/gui/vis.py
@@ -1,11 +1,27 @@
 """Utils for visualizing AML output using tkinter and pandastable"""
 
 import tkinter as tk
+from tkinter import filedialog
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from pandastable import Table
 
+
+class CSVSavingTable(Table):
+    def __init__(self, parent=None, **kwargs):
+        Table.__init__(self, parent, **kwargs)
+
+    def saveAs(self, filename=None):
+        """Save dataframe to file"""
+
+        if filename is None:
+            filename = filedialog.asksaveasfilename(parent=self.master,
+                                                    initialdir = self.currentdir,
+                                                    filetypes=[("csv","*.csv"),
+                                                               ("All files","*.*")])
+        if filename:
+            super().saveAs(filename)
 
 class TableVisualization:
     """Visualize tables in tkinter"""
@@ -13,8 +29,8 @@ class TableVisualization:
         self.frame = tk.Frame(root)
         self.frame.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
 
-        self.table = Table(self.frame, dataframe=df, showtoolbar=True,
-                           showstatusbar=True)
+        self.table = CSVSavingTable(self.frame, dataframe=df, showtoolbar=True,
+                                    showstatusbar=True)
         self.table.show()
 
 
