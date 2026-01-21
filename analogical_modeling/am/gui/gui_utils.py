e9d2d631ce375a15979d01ccac47aae7f40cde40
wiese <wiese@cl.uni-heidelberg.de>
2026-01-21 20:15:05 +0100

    Try to align GUI elements

--- analogical_modeling/am/gui/gui_utils.py
+++ analogical_modeling/am/gui/gui_utils.py
@@ -9,6 +9,7 @@ import pandas as pd
 from TkToolTip import ToolTip as Tip
 
 LEN = 40
+FLEN = LEN*8
 pack_config = {"side": tk.LEFT, "expand": True}  # , "fill": tk.X}
 
 
@@ -59,7 +60,9 @@ class ClsFrame(VisOnCommandFrame):
     def __init__(self, parent: tk.Frame, wrapper):
         super().__init__(parent, {"side": tk.TOP, "fill": tk.X, "expand": True})
         tk.Label(self.frame,
-                 text=f"{'Class column:':{LEN}s}\t").pack(side=tk.LEFT,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
+                 text=f"{'Class column:':{LEN*2}s}\t").pack(side=tk.LEFT,
                                                           expand=True,
                                                           fill=tk.X)
         self.box = ttk.Combobox(self.frame,
@@ -80,11 +83,11 @@ class ClsFrame(VisOnCommandFrame):
 
 
 class IgnoreFrame(VisOnCommandFrame):
-
-
     def __init__(self, parent: tk.Frame):
         super().__init__(parent, {"side": tk.TOP, "fill": tk.X, "expand": True})
         tk.Label(self.frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
                  text=f"{'Select columns to ignore:':{LEN}s}\t").pack(
             side=tk.LEFT,
             expand=True,
@@ -119,6 +122,8 @@ class WeightsFrame(VisOnCommandFrame):
 
         # weights_frame = tk.Frame(self.frame)
         tk.Label(self.frame,
+                 anchor=tk.W,
+                 justify=tk.LEFT,
                  text=f"{'Select weights column (if any):':{LEN}s}\t").pack(
             side=tk.LEFT, expand=True, fill=tk.X)
 
@@ -161,6 +166,8 @@ class ThresholdFrame(VisOnCommandFrame):
                          {"side": tk.BOTTOM, "fill": tk.X, "expand": True})
 
         tk.Label(self.frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
                  text=f"{'Ignore instances with weights below:':{LEN}s}\t").pack(
             side=tk.LEFT, expand=True, fill=tk.X)
         tk.Spinbox(self.frame, increment=0.01, from_=0.0, to=1.0,
@@ -184,7 +191,7 @@ class ThresholdFrame(VisOnCommandFrame):
 class OutFrame:
     def __init__(self, parent: tk.Frame, wrapper):
         self.frame = tk.Frame(parent)
-        self.frame.pack(expand=True, fill=tk.X)
+        self.frame.pack(expand=True, fill=tk.BOTH, side=tk.BOTTOM)
         # tk.Label(self.frame, text="Output specification", font=("", 11),
         #          pady=8).pack(side=tk.TOP, expand=True, fill=tk.X)
         self.wrapper = wrapper
@@ -192,16 +199,20 @@ class OutFrame:
         dir_frame = tk.Frame(self.frame)
         dir_frame.pack(expand=True, fill=tk.X)
         tk.Label(dir_frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
                  text=f"{'Output directory:':{LEN}s}\t").pack(side=tk.LEFT,
                                                               expand=True,
                                                               fill=tk.X)
         self.button = tk.Button(dir_frame, text="Select directory",
-                                command=self.get_out_dir)
-        self.button.pack(side=tk.RIGHT, expand=True, fill=tk.X)
+                                command=self.get_out_dir, width=LEN)
+        self.button.pack(side=tk.RIGHT, expand=True)
 
         prefix_frame = tk.Frame(self.frame)
-        prefix_frame.pack(expand=True, fill=tk.X)
+        prefix_frame.pack(side=tk.BOTTOM, after=dir_frame, expand=True, fill=tk.X)
         tk.Label(prefix_frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
                  text=f"{'Prefix for output:':{LEN}s}\t").pack(side=tk.LEFT,
                                                                expand=True,
                                                                fill=tk.X)
@@ -214,7 +225,7 @@ class OutFrame:
         self.label = tk.Label(self.frame,
                               text="Results won't be saved automatically.",
                               fg="red")
-        self.label.pack(side=tk.RIGHT, expand=True, fill=tk.X)
+        self.label.pack(side=tk.BOTTOM, before=prefix_frame, expand=True, fill=tk.X)
 
     def update_label_text(self, _arg=None):
         """Update text of label according to specified directory and name"""
@@ -237,27 +248,119 @@ class OutFrame:
 
         self.wrapper.out_dir = tmp
         self.update_label_text()
-        self.button.configure(text=f"Selected: */{Path(tmp).name}/")
+        text = f"Selected: */{Path(tmp).name}/"
+        self.button.configure(text=f'{text:^{LEN*2}s}')
+
+
+class MainConfigFrame:
+    def __init__(self, app, parent, wrapper):
+        self.app = app
+        self.parent = parent
+        self.wrapper = wrapper
+
+        tk.Label(parent, text="Configuration", font=("", 15)).pack(
+            expand=True,
+            fill=tk.BOTH)
+
+        # Lexicon
+        lex_spec_frame = tk.Frame(parent, width=FLEN)
+        lex_spec_frame.pack(fill=tk.Y, expand=True)
+
+        lex_frame = tk.Frame(lex_spec_frame)
+        lex_frame.pack(expand=True, fill=tk.X)
+        tk.Label(lex_frame, text=f"{'Lexicon file:':{LEN}s}\t", justify=tk.LEFT,
+                 anchor=tk.W).pack(side=tk.LEFT, expand=True, fill=tk.X)
+        self.lexicon_button = tk.Button(lex_frame, text="Select file",
+                                        command=self.get_lexicon, width=LEN)
+        self.lexicon_button.pack(side=tk.LEFT, expand=True)
+
+        # configurations once lexicon has been selected
+        conf_frame = tk.Frame(lex_spec_frame)
+        conf_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
+
+        self.cls = ClsFrame(conf_frame, wrapper)
+        self.ignored = IgnoreFrame(conf_frame)
+        self.weights = WeightsFrame(conf_frame, wrapper)
+
+        # Test set
+        test_frame = tk.Frame(lex_spec_frame)
+        test_frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+        tk.Label(test_frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
+                 text=f"{'Test file (optional):':{LEN}s}").pack(side=tk.LEFT,
+                                                                expand=True,
+                                                                fill=tk.X)
+        self.test_button = tk.Button(test_frame, text="Select file", width=LEN,
+                                     command=self.get_testset)
+        self.test_button.pack(side=tk.RIGHT, expand=True)
+        self.del_test_button = tk.Button(test_frame, text="Clear file",
+                                         command=self.clear_test, width=10)
+
+        # Output
+        OutFrame(lex_spec_frame, wrapper)
+
+    @staticmethod
+    def get_file(button):
+        filename = filedialog.askopenfilename(initialdir="./..",
+                                              title="Select a File",
+                                              filetypes=(("CSV files",
+                                                          "*.csv"),
+                                                         ("all files",
+                                                          "*.*")))
+        if not filename:
+            return None
+        # Change label contents
+        button.configure(text=f"Selected: {Path(filename).name}")
+        return filename
+
+    def get_lexicon(self):
+        lex = self.get_file(self.lexicon_button)
+        if not lex or not Path(lex).exists():
+            return
+        self.wrapper.lexicon = lex
+        cols = list(pd.read_csv(self.wrapper.lexicon).columns)
+
+        self.cls.fill(cols)
+        self.cls.vis()
+
+        self.ignored.clear()  # remove old elements
+        self.ignored.fill(cols)  # refill
+        self.ignored.vis()
+
+        self.weights.fill(cols)
+        self.weights.vis()
+        self.weights.threshold_frame.invis()  # as no weight column selected
+
+    def get_testset(self):
+        self.wrapper.testset = self.get_file(self.test_button) or ""
+        if self.wrapper.testset:
+            self.del_test_button.pack(side=tk.RIGHT, after=self.test_button, expand=True, fill=tk.X)
+
+    def clear_test(self):
+        self.wrapper.testset = ""
+        self.del_test_button.pack_forget()
+        self.test_button.configure(text="Select file", width=LEN)
 
 
 class CountFrame:
     """Frame to determine count strategy"""
     def __init__(self, parent, wrapper):
         self.frame = tk.Frame(parent)
-        self.frame.pack(expand=True)
+        self.frame.pack(expand=True, fill=tk.BOTH)
 
         tk.Label(self.frame,
-                 text="Count strategy:",
-                 padx=15).pack(side=tk.LEFT, expand=True, fill=tk.X)
+                 text=f"{'Count strategy:':{LEN}s}\t", justify=tk.LEFT,
+                 anchor=tk.W).pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         q = tk.Radiobutton(self.frame,
-                           text="quadratic count",
+                           text=f"{'quadratic count':{LEN//2}s}\t",
                            variable=wrapper.linear,
                            value=0)
         q.pack(side=tk.LEFT, expand=True, fill=tk.X)
         ToolTip(q,
                 "Count pointers within homogeneous supracontexts quadratically")
         l = tk.Radiobutton(self.frame,
-                           text="linear count",
+                           text=f"{'linear count':{LEN//2}s}\t",
                            variable=wrapper.linear,
                            value=1)
         l.pack(side=tk.LEFT, expand=True, fill=tk.X)
--- analogical_modeling/gui.py
+++ analogical_modeling/gui.py
@@ -32,15 +32,6 @@ root.title('Analogical Modeling')
 root.minsize(600, 700)
 root.geometry("800x700+50+50")
 
-notebook = ttk.Notebook(root)
-notebook.pack(fill=tk.BOTH, expand=True)
-
-main_tab = ttk.Frame(notebook, padding=10)
-notebook.add(main_tab, text="Configuration")
-
-tk.Label(main_tab, text="Configuration", font=("", 15)).pack(expand=True,
-                                                             fill=tk.BOTH)
-
 wrapper = AMWrapper()
 
 
@@ -48,15 +39,67 @@ class GUI:
     def __init__(self, root):
         self.root = root
 
+        self.notebook = ttk.Notebook(root)
+        self.notebook.pack(fill=tk.BOTH, expand=True)
+
+        main_tab = ttk.Frame(self.notebook, padding=10)
+        self.notebook.add(main_tab, text="Configuration")
+
+        self.conf = utils.MainConfigFrame(self, main_tab, wrapper)
+
+        ttk.Separator(main_tab, orient="horizontal").pack(expand=True,
+                                                          fill=tk.BOTH)
+
+        self.outs = utils.OutputSelection(main_tab)
+
+        ttk.Separator(main_tab, orient="horizontal").pack(expand=True,
+                                                          fill=tk.BOTH)
+
+        # additional options
+        tk.Label(main_tab, text="Additional Options", pady=10,
+                 font=("", 13)).pack(
+            expand=True,
+            fill=tk.X)
+        options_frame = tk.Frame(main_tab, width=utils.FLEN)
+        options_frame.pack(expand=True, fill=tk.Y)
+
+        # count strategy
+        self.count = utils.CountFrame(options_frame, wrapper)
+
+        # mdc
+        mdc_frame = tk.Frame(options_frame, width=utils.LEN * 2)
+        mdc_frame.pack(expand=True, fill=tk.BOTH)
+        tk.Label(mdc_frame,
+                 justify=tk.LEFT,
+                 anchor=tk.W,
+                 text=f"{'Consider missing values as:':{utils.LEN}s}\t").pack(
+            side=tk.LEFT,
+            expand=True, fill=tk.X)
+        mdc_selection = ttk.Combobox(mdc_frame,
+                                     values=["match", "mismatch", "variable"],
+                                     textvariable=wrapper.mdc, state="readonly")
+        mdc_selection.current(2)
+        mdc_selection.pack(side=tk.LEFT, expand=True, anchor=tk.W)
+        utils.ToolTip(mdc_selection,
+                      "The strategy to use when comparing missing attribute values "
+                      "\nwith other values while filling subcontexts and supracontexts")
+
+        # rest
+        rest_frame = tk.Frame(options_frame)
+        rest_frame.pack(expand=True, anchor=tk.CENTER)  # don't fill
+        utils.CheckboxFrames(rest_frame, wrapper)
+
+        self.run_stop = utils.StartFrame(self, main_tab, wrapper)
+
     def run(self):
         """Run AML through wrapper"""
         # runs in separate thread
         try:
             print(wrapper.weights)
-            wrapper.class_idx = cls.get()
-            wrapper.ignored = ignored.list_selected()
+            wrapper.class_idx = self.conf.cls.get()
+            wrapper.ignored = self.conf.ignored.list_selected()
 
-            if wrapper.class_idx in ignored.box.curselection():
+            if wrapper.class_idx in self.conf.ignored.box.curselection():
                 messagebox.showerror("Class column ignored",
                                      "The class column cannot be ignored.")
                 return False
@@ -64,7 +107,7 @@ class GUI:
                 messagebox.showerror("Weights column ignored",
                                      "The weights column cannot be ignored.")
                 return False
-            if wrapper.weights == cls.get(wrapper.class_idx):
+            if wrapper.weights == self.conf.cls.get(wrapper.class_idx):
                 messagebox.showerror("Weights column is class column",
                                      "The weights column cannot be the class "
                                      "column.")
@@ -103,8 +146,8 @@ class GUI:
 
     def vis_matrix(self, matrix, acc):
         if not hasattr(root, "conf_mat_tab"):
-            root.conf_mat_tab = ttk.Frame(notebook)
-            notebook.add(root.conf_mat_tab, text="Confusion Matrix")
+            root.conf_mat_tab = ttk.Frame(self.notebook)
+            self.notebook.add(root.conf_mat_tab, text="Confusion Matrix")
         else:
             # Update existing tab
             self.clear_frame(root.conf_mat_tab)
@@ -120,145 +163,36 @@ class GUI:
 
     def vis_files(self, files):
         """Visualize tables"""
-        if outs.gangs.get():
+        if self.outs.gangs.get():
             if not hasattr(root, "gangs"):
-                root.gangs = ttk.Frame(notebook)
-                notebook.add(root.gangs, text="Gang Effects")
+                root.gangs = ttk.Frame(self.notebook)
+                self.notebook.add(root.gangs, text="Gang Effects")
             else:
                 # Update existing tab
                 self.clear_frame(root.gangs)
             self.make_table(root.gangs, files[0])
 
-        if outs.analog.get():
+        if self.outs.analog.get():
             if not hasattr(root, "analog"):
-                root.analog = ttk.Frame(notebook)
-                notebook.add(root.analog, text="Analogical Sets")
+                root.analog = ttk.Frame(self.notebook)
+                self.notebook.add(root.analog, text="Analogical Sets")
             else:
                 # Update existing tab
                 self.clear_frame(root.analog)
             self.make_table(root.analog, files[1])
 
-        if outs.distr.get():
+        if self.outs.distr.get():
             if not hasattr(root, "distr"):
-                root.distr = ttk.Frame(notebook)
-                notebook.add(root.distr, text="Distribution")
+                root.distr = ttk.Frame(self.notebook)
+                self.notebook.add(root.distr, text="Distribution")
             else:
                 # Update existing tab
                 self.clear_frame(root.distr)
             self.make_table(root.distr, files[2])
 
 
-gui = GUI(root)
-
-
-def get_file(button):
-    filename = filedialog.askopenfilename(initialdir="./..",
-                                          title="Select a File",
-                                          filetypes=(("CSV files",
-                                                      "*.csv"),
-                                                     ("all files",
-                                                      "*.*")))
-    if not filename:
-        return None
-    # Change label contents
-    button.configure(text=f"Selected: {Path(filename).name}")
-    return filename
-
-
-def get_lexicon():
-    wrapper.lexicon = get_file(lexicon_button)
-    if not wrapper.lexicon or not Path(wrapper.lexicon).exists():
-        return
-    cols = list(pd.read_csv(wrapper.lexicon).columns)
-
-    cls.fill(cols)
-    cls.vis()
-
-    ignored.clear()  # remove old elements
-    ignored.fill(cols)  # refill
-    ignored.vis()
-
-    weights.fill(cols)
-    weights.vis()
-    weights.threshold_frame.invis()  # as no weight column selected
-
-
-def get_testset():
-    wrapper.testset = get_file(test_button) or ""
-    if wrapper.testset:
-        del_test_button.pack(side=tk.RIGHT, after=test_button)
-
-
-def clear_test():
-    wrapper.testset = ""
-    del_test_button.pack_forget()
-    test_button.configure(text="Select file")
-
-
-# Lexicon
-lex_spec_frame = tk.Frame(main_tab)
-lex_spec_frame.pack(fill=tk.BOTH, expand=True)
-lex_frame = tk.Frame(lex_spec_frame)
-lex_frame.pack(expand=True, fill=tk.X)
-tk.Label(lex_frame, text="Lexicon file:").pack(side=tk.LEFT, expand=True,
-                                               fill=tk.X)
-lexicon_button = tk.Button(lex_frame, text="Select file", command=get_lexicon)
-lexicon_button.pack(side=tk.RIGHT, expand=True, fill=tk.X)
-
-# configurations once lexicon has been selected
-conf_frame = tk.Frame(lex_spec_frame)
-conf_frame.pack(side=tk.BOTTOM, expand=True, fill=tk.X)
-cls = utils.ClsFrame(conf_frame, wrapper)
-ignored = utils.IgnoreFrame(conf_frame)
-weights = utils.WeightsFrame(conf_frame, wrapper)
-
-# Test set
-test_frame = tk.Frame(main_tab)
-test_frame.pack(expand=True, fill=tk.X)
-tk.Label(test_frame, text="Test file (optional):").pack(side=tk.LEFT,
-                                                        expand=True, fill=tk.X)
-test_button = tk.Button(test_frame, text="Select file", command=get_testset)
-test_button.pack(side=tk.RIGHT, expand=True, fill=tk.X)
-del_test_button = tk.Button(test_frame, text="Clear file", command=clear_test)
-
-# Output
-utils.OutFrame(main_tab, wrapper)
-
-ttk.Separator(main_tab, orient="horizontal").pack(expand=True, fill=tk.BOTH)
-
-outs = utils.OutputSelection(main_tab)
-
-ttk.Separator(main_tab, orient="horizontal").pack(expand=True, fill=tk.BOTH)
-
-tk.Label(main_tab, text="Additional Options", pady=10, font=("", 13)).pack(
-    expand=True,
-    fill=tk.X)
-
-# count strategy
-count = utils.CountFrame(main_tab, wrapper)
-
-# mdc
-mdc_frame = tk.Frame(main_tab)
-mdc_frame.pack(expand=True, fill=tk.X)
-tk.Label(mdc_frame, text="Consider missing values as:").pack(side=tk.LEFT,
-                                                             expand=True,
-                                                             fill=tk.X)
-mdc_selection = ttk.Combobox(mdc_frame,
-                             values=["match", "mismatch", "variable"],
-                             textvariable=wrapper.mdc, state="readonly")
-mdc_selection.current(2)
-mdc_selection.pack(side=tk.LEFT, expand=True)
-utils.ToolTip(mdc_selection,
-              "The strategy to use when comparing missing attribute values "
-              "\nwith other values while filling subcontexts and supracontexts")
-
-# rest
-rest_frame = tk.Frame(main_tab)
-rest_frame.pack(expand=True, anchor=tk.CENTER)  # don't fill
-utils.CheckboxFrames(rest_frame, wrapper)
-
-run_stop = utils.StartFrame(gui, main_tab, wrapper)
-
-# FIXME: pops up at base position first
-# center_window(root)
-root.mainloop()
+if __name__ == "__main__":
+    # FIXME: pops up at base position first
+    # center_window(root)
+    GUI(root)
+    root.mainloop()
